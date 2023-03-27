2e2ebc2cb405c2435eeff216f65a1b6317828a67
unknown <alina.shabeka.pl@gmail.com>
2023-03-27 11:50:07 +0200

    hm1.py

---
+++ hm1.py
@@ -0,0 +1,25 @@
+print("Hello, world!")
+
+# Write a program that takes the username as input, stores it in the user_name variable, and outputs the string "Hello {user_name}!"
+user_name = input()
+print(f"Hello {user_name}!")
+
+#Write a program that takes 2 numbers as input, performs an arithmetic operation with them (at your discretion), and displays “Result = {result}”.
+num1 = int(input())
+num2 = int(input())
+num3 = num1 + num2
+print(f"Result: = {num3}")
+
+#Write a program to output:
+print("*********")
+print("*       *")
+print("*       *")
+print("*********")
+
+#or_version_2
+print("*********\n*       *\n*       *\n*********")
+
+
+
+
+
